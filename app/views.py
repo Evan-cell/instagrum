1302e92b4e63cd1c047f1b2f04250153b150ce95
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-12-06 12:18:33 +0300

    signup and login

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from.forms import CreateUserForm
+from django.contrib import messages
 # Create your views here.
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
@@ -15,6 +16,9 @@ def registerPage(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+			
             return redirect('login')
     context = {'form': form}
     return render(request, 'accounts/register.html', context)
