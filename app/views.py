f30291074fc18d9ec69f988e5523733e915238bb
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-12-06 10:39:20 +0300

    form

--- app/views.py
+++ app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+
+# Create your views here.
+def home(request):
+   return render(request, 'base/home.html')
+def registerPage(request):
+    form = UserCreationForm()
+    context = {'form': form}
+    return render(request, 'accounts/register.html', context)
+def loginPage(request):
+    context = {}
+    return render(request, 'accounts/login.html', context)
