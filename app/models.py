2a7b9393e8fc40e21b3e4a2625d293e0306ad59a
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-12-07 17:48:12 +0300

    comments

--- app/models.py
+++ app/models.py
@@ -153,3 +153,29 @@ class StoryStream(models.Model):
 
 # Story Stream
 post_save.connect(StoryStream.add_post, sender=Story)
+
+# comment
+
+class Comment(models.Model):
+	post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	body = models.TextField()
+	date = models.DateTimeField(auto_now_add=True)
+
+	def user_comment_post(sender, instance, *args, **kwargs):
+		comment = instance
+		post = comment.post
+		text_preview = comment.body[:90]
+		sender = comment.user
+		
+
+	def user_del_comment_post(sender, instance, *args, **kwargs):
+		like = instance
+		post = like.post
+		sender = like.user
+
+		
+
+#Comment
+post_save.connect(Comment.user_comment_post, sender=Comment)
+post_delete.connect(Comment.user_del_comment_post, sender=Comment)
